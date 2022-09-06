97308a1bd0bd2abbbb87ff8948b887f8676c48d4
anne0121 <106682768+anne0121@users.noreply.github.com>
2022-09-06 20:14:04 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 import time
 token ="1612896303:AAGPhZpdMDuiVSZG5XloXZT13j8FjqiDkmI" 
 while True:
-   print(time.time())
-   get= requests.get("https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}".format(token,960317749,str(time.time())))
-   time.sleep(2)
+   t=time.strftime("%I:%M:%S")
+   print(t)
+   get= requests.get("https://api.telegram.org/bot{0}/sendMessage?chat_id={1}&text={2}".format(token,960317749,t))
+   time.sleep(60)
